13e7bae5107d95b4b7e1652076022677f52b0f5d
randy <modulloe@gmail.com>
2019-06-16 20:34:27 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyCard());
+
+class MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/images/randy.jpg'),
+              ),
+              Text('Randy Vianda Putra',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                'FRONTEND ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.lightBlue.shade100,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.lightBlue.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.lightBlue),
+                  title: Text(
+                    '+62 857 9399 1997',
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.lightBlue,
+                  ),
+                  title: Text(
+                    'modulloe@gmail.com',
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
